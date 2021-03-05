f9aa1fd20da2dc53eb258248bf9e1aa012e098e8
Wolfgang Rumpler <contact@iflow.space>
2021-03-05 17:02:07 +0100

    sierpinsky pyramid/tetrahedron

--- src/rendering/octree.rs
+++ src/rendering/octree.rs
@@ -18,8 +18,7 @@ use std::result::Result;
 use std::sync::{Arc, Mutex};
 use std::time::{Duration, Instant};
 
-use cgmath::{Array, ElementWise, Matrix3, Matrix4, Transform, vec3, Vector3, Rotation, InnerSpace};
-use cgmath::num_traits::Pow;
+use cgmath::{Array, Matrix4, Transform, vec3, Vector3, Rotation, InnerSpace};
 use gfx_hal::buffer;
 use winit::event::Event;
 
@@ -28,6 +27,7 @@ use crate::rendering::{
     AABB, Camera, Frustum, GPUBuffer, InstanceData, Mesh, RenderSystem, Transformation,
 };
 use crate::rendering::nse_gui::octree_gui::ProfilingData;
+use cgmath::num_traits::Pow;
 
 enum NodePosition {
     Flt = 0,
@@ -164,7 +164,7 @@ impl Octree {
 
         oct.root.lock().unwrap().populate();
 
-        Arc::new(Mutex::new(Octree::traverse_mandelbrot(
+        Arc::new(Mutex::new(Octree::traverse_sierpinsky(
             &mut oct.root.lock().unwrap(),
             0,
             depth,
@@ -374,13 +374,96 @@ impl Octree {
                 });
         }
     }
+
+    fn traverse_sierpinsky(node: &mut Node, current_depth: u64, target_depth: u64) {
+        if node.children.is_some() {
+            node.children.as_mut().unwrap()
+                .iter_mut()
+                .enumerate()
+                .for_each(|(idx, child)| {
+                    let s = child.scale;
+                    let p = child.position;
+
+                    fn f1(p: Vector3<f32>, s: f32, max: i32) -> f32 {
+                        let a = (p.x + 0.5).abs();
+                        let b = (p.z + 0.5).abs();
+                        let c = (p.y + 0.5).abs();
+
+                        return (a + b + c) - 1.0;
+                    };
+
+                    fn iterate(p: Vector3<f32>, s: f32, bb_center: Vector3<f32>, bb_size: f32, n: i32) -> bool {
+                        if n == 0 { return true; }
+
+                        // bounding box of the current iteration/contraction
+                        let bb_min = bb_center - Vector3::from_value(bb_size);
+                        let bb_max = bb_center + Vector3::from_value(bb_size);
+
+                        // bounding box of node
+                        let node_min = p - Vector3::from_value(s);
+                        let node_max = p + Vector3::from_value(s);
+
+                        // test node bb and iteration bb intersection
+                        if node_max.x >= bb_min.x && node_min.x <= bb_max.x
+                            && node_max.y >= bb_min.y && node_min.y <= bb_max.y
+                            && node_max.z >= bb_min.z && node_min.z <= bb_max.z {
+
+                            // calculate contraction bounding size
+                            let c_size = bb_size * 0.5;
+
+                            // calculate next contraction positions
+                            // note: the actual iteration of the IFS
+                            // Pyramid (numerically more stable in octree)
+                            let bounding = [
+                                bb_center + vec3(-c_size, -c_size, c_size),
+                                bb_center + vec3(-c_size, -c_size, -c_size),
+                                bb_center + vec3(c_size, -c_size, -c_size),
+                                bb_center + vec3(c_size, -c_size, c_size),
+                                bb_center + vec3(0.0, c_size, 0.0),
+                            ];
+                            // Tetrahedron (less stable in octree)
+                            /*let c_size_diag = (c_size * c_size * 0.5).sqrt();
+                            let bounding = [
+                                bb_center + vec3(-c_size_diag, -c_size, c_size_diag),
+                                bb_center + vec3(c_size_diag, -c_size, c_size_diag),
+                                bb_center + vec3(0.0, -c_size, -c_size),
+                                bb_center + vec3(0.0, c_size, 0.0),
+                            ];*/
+
+                            // check if any of the next bounding volumes intersects with the node
+                            // if none does then we definitely have a node that we do not need
+                            // to consider anymore
+                            let inside = bounding.iter().any(|bb| {
+                                iterate(p, s, *bb, c_size, n-1)
+                            });
+
+                            return inside;
+                        }
+
+                        // they do not intersect
+                        return false;
+                    }
+
+                    let d = iterate(p, s * 0.5, vec3(0.0, 0.0,0.0), 0.5,15);
+
+                    if d  {
+                        child.solid = true;
+                    }
+
+                    if d && current_depth < target_depth {
+                        child.populate();
+                        Octree::traverse_sierpinsky(child, current_depth + 1, target_depth)
+                    }
+                });
+        }
+    }
 }
 
 #[derive(Clone, Debug)]
 pub struct Node {
     children: Option<Vec<Node>>,
-    position: Vector3<f32>,
-    scale: f32,
+    position: Vector3<f32>, // position of the node
+    scale: f32, // length of a side of the node
     solid: bool,
 }
 
